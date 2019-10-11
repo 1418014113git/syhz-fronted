5c38659874d6801217fb5b04e9ba85cbaed1147e
yanjing <123@qq.com>
2019-10-11 11:08:04 +0800

    放开 wmv 格式视频上传，统计字段不对应问题修改

--- src/views/microClass/uploadFile.vue
+++ src/views/microClass/uploadFile.vue
@@ -233,13 +233,14 @@
         }
       },
       fileBeforeUpload(file) {
+        console.info(file.type)
         this.uploadData.uploadUserId = this.curUser.id
         this.uploadData.uploadDate = this.$parseTime(new Date(), '{y}{m}{d}{h}{i}{s}')
         this.uploadData.fileName = file.name
         const wordReg = /^(application\/vnd\.openxmlformats-officedocument\.wordprocessingml\.document)|(application\/msword)$/
         const pdfReg = /^(application\/vnd.openxmlformats-officedocument.presentationml.presentation)|(application\/pdf)$/
         const pptReg = /^(application\/vnd.ms-powerpoint)$/
-        const videoReg = /^(video\/mp4)|(video\/avi)|(video\/wmv)$/
+        const videoReg = /^(video\/mp4)|(video\/avi)|(video\/x-ms-wmv)$/
         const audioReg = /^(audio\/mp3)$/
         let flag = false
         if (wordReg.test(file.type) || pdfReg.test(file.type) || pptReg.test(file.type)) {
