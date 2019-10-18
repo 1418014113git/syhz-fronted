0ffe55837497ecf00cd26610854584804cfbe3fc
”wuting“git config --global user.email “305842278@qq.com <305842278@qq.com>
2019-10-18 18:27:56 +0800

    1.删除一个多余的文件夹以及对应的路由。 2. 优化组卷分页选题不停留在当前页问题。 3. 登录页新增3张轮播图以及显示顺序调整。

--- src/views/handlingGuide/examPaperManage/addExamPaper.vue
+++ src/views/handlingGuide/examPaperManage/addExamPaper.vue
@@ -751,12 +751,13 @@ export default {
       }
     },
     getCheckList(val) { // 获取选择的试题列表
-      // this.initData(1)
       this.rgzjList = [] // 初始化数据
       this.rgzjStList = []
       if (val.length > 0) {
         val.forEach((item, index) => {
-          this.getshitiList(item)
+          if (item.data.length > 0) {
+            this.getshitiList(item)
+          }
         })
       }
     },
