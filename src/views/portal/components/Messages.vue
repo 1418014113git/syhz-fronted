4ab021a895e8051a43a018c48c9778e952b4175b
yanjing <123@qq.com>
2019-11-04 18:56:03 +0800

    消息中心，消息详情发送人没有时，显示-

--- src/views/portal/components/Messages.vue
+++ src/views/portal/components/Messages.vue
@@ -61,7 +61,7 @@
       <el-form :model="detail" label-width="100px" style="padding-left: 20px; padding-right: 15px;" v-loading="dialogLoading">
         <el-form-item label="消息内容">{{detail.content}}</el-form-item>
         <el-form-item label="发送时间">{{detail.creatorDate}}</el-form-item>
-        <el-form-item label="发送人">{{detail.creatorName}}</el-form-item>
+        <el-form-item label="发送人">{{detail.creatorName ? detail.creatorName : '-'}}</el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="closeDialog" class="cancelBtn">关闭</el-button>
